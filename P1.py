304a0213ee4dbff2174f3e07d8c5f937810f35e2
silviogrizzly <43226201+silviogrizzly@users.noreply.github.com>
2018-10-04 22:29:49 -0300

    Creacion Funciones

---
+++ P1.py
@@ -0,0 +1,15 @@
+#Creacion de ecuacion Catenaria para encontrar ceros
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Para la funcion catenaria en el enunciado se aplicó condiciones dadas para tener la funcion que entregará los ceros
+
+def f(x):  #funcion 
+    y=x*np.cosh(10/x)-x-7.5
+    return y
+
+def df(x):      #Derivada de la funcion f(x)
+    y=np.cosh(10/x)-((10/x)*np.sinh(10/x))-1
+    return y
+
